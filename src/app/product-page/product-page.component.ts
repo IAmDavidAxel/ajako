dec2910b1b9e8765cbb472c531383b28486b0baf
David Axel Nikyèma <david-axel-wendbenedo.nikyema.1@ulaval.ca>
2022-06-25 22:57:25 -0300

    added profile details and detailed product page

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-product-page',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
+  ReviewForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    review: new FormControl('', [Validators.required])
+  });
 
   constructor() { }
 
